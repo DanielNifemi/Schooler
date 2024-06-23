0079562581905817e07f92b6eae536f229e9c18f
nifemi daniel <103764624+Daniel22022@users.noreply.github.com>
2024-06-23 17:32:32 +0100

    Ensure psycopg2-binary is used and update settings

--- schooler/settings.py
+++ schooler/settings.py
@@ -6,7 +6,7 @@ https://docs.djangoproject.com/en/3.x/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.x/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'schooler.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'eHPBbUNHBibBIWXPazVRLbxScRALvJiZ',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '33654',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
